7c293b91580d7da2d34a38a53240cbf1130e952b
boomgreen87 <boomgreen87@yahoo.com>
2019-09-19 13:12:35 -0400

    Added user info

--- js/modules/DataObject.js
+++ js/modules/DataObject.js
@@ -1,7 +1,36 @@
 export default {
-    name: "Scott",
-    role: "Genius",
-    height: "6ft, 7ft, 8ft BUNCH",
-    Weight: "Heavyweight Champion",
-    hairColor: "Slim Shady Bleach Blonde"
+    Scott: {
+        name: "Scott",
+        role: "Genius",
+        bio: "Bout to run full speed off a cliff",
+        avatar: "ned.png"
+    },
+
+    Justin: {
+        name: "Justin",
+        role: "Professor / Coordinator",
+        bio: "That avatar is diss",
+        avatar: "justin.jpeg"
+    },
+
+    Joe: {
+        name: "Joe",
+        role: "Professor",
+        bio: "I'm Joe",
+        avatar: "olaf.jpeg"
+    },
+
+    Pan: {
+        name: "Pan",
+        role: "Professor",
+        bio: "Stop stereotyping me",
+        avatar: "pan.jpg"
+    },
+    
+    Trevor: {
+        name: "Trevor",
+        role: "Professor",
+        bio: "Sorry, I gotta leave",
+        avatar: "zoolander.jpg"
+    }
 }
